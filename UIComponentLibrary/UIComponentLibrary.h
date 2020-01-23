4026e29d90d3ad7032387839e67af5e971ff6c48
Amit Garg <amitg@ohumhealthcare.com>
2020-01-23 12:46:30 +0530

    Initial project setup

---
+++ UIComponentLibrary/UIComponentLibrary.h
@@ -0,0 +1,19 @@
+//
+//  UIComponentLibrary.h
+//  UIComponentLibrary
+//
+//  Created by Rakesh Patole on 23/01/20.
+//  Copyright © 2020 Amit Garg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIComponentLibrary.
+FOUNDATION_EXPORT double UIComponentLibraryVersionNumber;
+
+//! Project version string for UIComponentLibrary.
+FOUNDATION_EXPORT const unsigned char UIComponentLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIComponentLibrary/PublicHeader.h>
+
+
---
+++ UIComponentLibraryDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIComponentLibraryDemo
+//
+//  Created by Rakesh Patole on 23/01/20.
+//  Copyright © 2020 Amit Garg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
